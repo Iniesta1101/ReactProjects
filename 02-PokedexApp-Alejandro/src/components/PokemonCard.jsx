c19687fe51b41de80127ab1fc58f8fa130571778
Lalo <madridcommunity9@gmail.com>
2024-01-18 19:27:31 +0100

    Cartas terminadas

--- 02-PokedexApp-Alejandro/src/components/PokemonCard.jsx
+++ 02-PokedexApp-Alejandro/src/components/PokemonCard.jsx
@@ -8,10 +8,13 @@ export default function PokemonCard(){
         let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
         let datosPokemon = await pokemon.json();
         
+        const types = datosPokemon.types.map(tipo => tipo.type.name);
+
         return {
             id: datosPokemon.id,
             name: datosPokemon.name,
-            img: datosPokemon.sprites.other.dream_world.front_default
+            img: datosPokemon.sprites.other.dream_world.front_default,
+            types: types
         };
     }
     useEffect(() => {
@@ -40,12 +43,20 @@ export default function PokemonCard(){
     return <>{
         
         pokemons.map(pokemon => 
-            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 justify-content-center d-flex align-items-center mt-3">
-                <div key={pokemon.id} class="card" style={{ width: '18rem' }}>
+            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 justify-content-center d-flex align-items-center mt-5" key={pokemon.id}>
+                <div key={pokemon.id} className={`card card-shadow ${pokemon.types[0]}`} style={{ width: '18rem' }}>
                     <img src={pokemon.img} className="card-img-top card-img mx-auto mt-3" alt={`Imagen de ${pokemon.name}`}></img>
                     <div className="card-body">
+                        <h3 className="card-title">{pokemon.name}</h3>
                         <p>{pokemonId(pokemon.id)}</p>
-                        <h5 className="card-title">{pokemon.name}</h5>
+                        <div className="d-flex">
+                        {pokemon.types.map((type, index) => (
+                            <div class={`badge ${type}2 d-flex me-2 align-items-center`}>
+                                <img src={`../src/assets/types-icons/${type}.svg`} className="type-img"/>
+                                <p key={index} className="me-4 m-0 ms-2">{type}</p>
+                            </div> 
+                        ))}
+                        </div>
                     </div>
                 </div>
             </div> 
--- 02-PokedexApp-Alejandro/src/components/Template.jsx
+++ 02-PokedexApp-Alejandro/src/components/Template.jsx
@@ -1,11 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import PokemonCard from './PokemonCard.jsx';
+import './Template.css';
 
 export default function Template(){
     return(
         <>
             <div className="container-fluid">
                 <div className="row justify-content-center">
+                <img src="../src/assets/title.png" className="img mt-4"/>
+                </div>
+                
+                <div className="row justify-content-center">    
                     <PokemonCard/>
                 </div>
             </div>
