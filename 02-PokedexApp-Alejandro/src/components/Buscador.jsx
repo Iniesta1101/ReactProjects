fba12edcbf6840f82a2e58b67202ccc697b7f049
Lalo <madridcommunity9@gmail.com>
2024-01-19 18:12:05 +0100

    Buscador

---
+++ 02-PokedexApp-Alejandro/src/components/Buscador.jsx
@@ -0,0 +1,18 @@
+
+export default function Buscador(){
+    
+    return(<>
+        <div className="row">
+            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 justify-content-center d-flex mt-5">
+                <form id="searchForm">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Encuentra tu pokémon" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                        <button class="btn btn-primary" type="submit" id="button-addon2"><img src="../src/assets/search.svg"/></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </>)
+    
+    
+}
--- 02-PokedexApp-Alejandro/src/components/Template.jsx
+++ 02-PokedexApp-Alejandro/src/components/Template.jsx
@@ -1,16 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import PokemonCard from './PokemonCard.jsx';
 import './Template.css';
+import Buscador from "./Buscador.jsx";
 
 export default function Template(){
     return(
         <>
             <div className="container-fluid">
-                <div className="row justify-content-center">
-                <img src="../src/assets/title.png" className="img mt-4"/>
+                <div className="row justify-content-center mt-4">
+                    <img src="../src/assets/title.png" className="img"/>
                 </div>
-                
-                <div className="row justify-content-center">    
+                <Buscador/>
+                <div className="row">    
                     <PokemonCard/>
                 </div>
             </div>
